c9a275bbef1fe9c5b9cbbc9c05c809db908429c3
Arsen Grigoryan <happysmilik@gmail.com>
2021-12-01 20:22:42 +0300

    Initial commit

---
+++ src/main/java/CommentsInfo.java
@@ -0,0 +1,10 @@
+public class CommentsInfo {
+    private String commentatorName;
+    private String commentText;
+    private String dateTime;
+    private String answer;
+    private String repost;
+    private String likeCounter;
+    private String answerCounter;
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,18 @@
+public class Post {
+    private String id;
+    private String communityName;
+    private String date;
+    private String contextMenu;
+    private String postText;
+    private String postImage;
+    private boolean addToBookmarks;
+    private String likeCounter;
+    private boolean isLike;
+    private CommentsInfo commentsInfo;
+    private String repostCounter;
+    private boolean isRepost;
+    private String viewCounter;
+
+    //getters + setters;
+
+}
